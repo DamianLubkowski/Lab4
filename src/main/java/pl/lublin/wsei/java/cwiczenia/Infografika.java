e5e234a0d5b7cc2b5ec187a2b28f45becdf8ce8e
DamianLubkowski <multiclick@wp.pl>
2022-09-22 20:21:43 +0200

    Punkt 11d

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Infografika.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Infografika.java
@@ -1,5 +1,8 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Infografika {
     public String tytul;
     public String adresStrony;
@@ -8,7 +11,62 @@ public class Infografika {
     public Integer szerokosc;
     public Integer wysokosc;
 
-    public Infografika(String text) {
+    public Infografika(String tekst) {
+        Pattern pat = Pattern.compile("<title><!\\[CDATA\\[(.*)\\]\\]<\\/title>");
+        Matcher m = pat.matcher(tekst);
+        if (m.find())
+            tytul = m.group(1);
+        else
+            tytul = "";
+
+        System.out.println(tytul);
+
+        pat = Pattern.compile("<link>(.*)</link>");
+        m = pat.matcher(tekst);
+        if(m.find())
+            adresStrony = m.group(1);
+        else
+            adresStrony = "";
+
+        System.out.println(adresStrony);
+
+        pat = Pattern.compile("<media:content url=\"(.*)\\\" type=\"image");
+        m = pat.matcher(tekst);
+        if(m.find())
+            adresGrafiki = m.group(1);
+        else
+            adresGrafiki = "";
+
+        System.out.println(adresGrafiki);
+
+        pat = Pattern.compile("<media:thumbnail url=\"(.*).(.png)\"");
+        m = pat.matcher(tekst);
+        if(m.find())
+            adresMiniaturki = m.group(1);
+        else
+            adresMiniaturki = "";
+
+        System.out.println(adresMiniaturki);
+
+        pat = Pattern.compile("width=\"(.*)\" height");
+        m = pat.matcher(tekst);
+
+        if (m.find())
+            szerokosc = Integer.parseInt(m.group(1));
+        else
+            szerokosc = 0;
+
+        System.out.println(szerokosc);
+
+        pat = Pattern.compile("height=\"(\\d*)\">");
+        m = pat.matcher(tekst);
+
+        if (m.find())
+            wysokosc = Integer.parseInt(m.group(1));
+        else
+            wysokosc = 0;
+
+        System.out.println(wysokosc);
 
     }
 }
