f730a101ef204db917996766b576d3c583aa6c88
DamianLubkowski <multiclick@wp.pl>
2022-09-22 09:27:54 +0200

    Punkt 9, utworzenie klasy Infografika

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Infografika.java
@@ -0,0 +1,12 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+public class Infografika {
+    public String tytul;
+    public String adresStrony;
+    public String adresGrafiki;
+    public String adresMiniaturki;
+    public Integer szerokosc;
+    public Integer wysokosc;
+
+
+}
